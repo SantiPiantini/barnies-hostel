dbfdb1aaacbdbe4375766e4f113699a91b86ce7f
santino <santinopiantini@gmail.com>
2025-06-12 15:06:52 -0300

    first userController

---
+++ src/main/java/controllers/User/UserController.java
@@ -0,0 +1,20 @@
+package controllers.User;
+
+import models.User.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ResponseEntity;
+import services.User.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    public UserService getReserveService() {
+    }
+}
