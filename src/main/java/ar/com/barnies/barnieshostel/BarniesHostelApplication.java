5a8cc7610a9bbb86ec8ffe3596e7cc555532ecbc
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 <lautaroolmedo77@gmail.com>
2025-06-12 12:00:39 -0300

    first

---
+++ src/main/java/ar/com/barnies/barnieshostel/BarniesHostelApplication.java
@@ -0,0 +1,13 @@
+package ar.com.barnies.barnieshostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarniesHostelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarniesHostelApplication.class, args);
+    }
+
+}
